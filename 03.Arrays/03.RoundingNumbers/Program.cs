2d44a0a78e28546c0efd14b8c23811e12eaffb8d
Georgi Inkov <georgi.inkov@scaleforce.io>
2024-09-25 22:18:36 +0300

    Remove unused array

--- 03.Arrays/03.RoundingNumbers/Program.cs
+++ 03.Arrays/03.RoundingNumbers/Program.cs
@@ -12,15 +12,11 @@ internal class Program
             .Select(double.Parse)
             .ToArray();
 
-        int[] roundedNumbers = new int[numbers.Length];
-
         for (int i = 0; i < numbers.Length; i++)
         {
             int roundedNumber = (int)Math.Round(numbers[i], MidpointRounding.AwayFromZero);
 
-            roundedNumbers[i] = roundedNumber;
-
-            Console.WriteLine($"{numbers[i]} => {roundedNumbers[i]}");
+            Console.WriteLine($"{numbers[i]} => {roundedNumber}");
         }
     }
 }
